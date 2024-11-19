437146346f5436af658e4fdf15fad0fbf0219a87
FrancescaFrisoni <124571317+FrancescaFrisoni@users.noreply.github.com>
2024-11-19 14:10:18 +0100

    new function

--- R/cesca.R
+++ R/cesca.R
@@ -1,13 +1,23 @@
 cesca_function <- function(word){
 
-  # Find input word in wordlist
   input_index <- which(all_english_words == word)
 
 
   if(length(input_index) == 0) {
     print("Bummer, not a word")
   } else {
-    print("Word found in list, congrats!")
+    print("Word found in list")
   }
-return(word)
+
+  if(length(input_index) != 0) {
+    print(paste0(word," is a great word! What about ", all_english_words[input_index + 1], "? And all the other words that start with ", word, "?"))
+  }
+
+  # all words that start with the given word
+  matching_words <- all_english_words[startsWith(all_english_words, word)]
+
+  # list of matching words
+  return(matching_words)
+
+
 }
