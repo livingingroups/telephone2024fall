1337bc2e46d5b7e1178b10c6e8ebda146c7c29f0
Brock <kbrock@ab.mpg.de>
2024-11-19 14:40:30 +0100

    Update sonya.R

--- R/sonya.R
+++ R/sonya.R
@@ -1,6 +1,7 @@
 sonya_function <- function(word){
   input_index <- which(all_english_words == word)
 
+  # only one song???? no other songs???
   output <- paste("it's a cooold and it's a brooooken", all_english_words[1991])
   if(length(input_index) == 0) return(output)
   else return(all_english_words[input_index[1] + 5])
