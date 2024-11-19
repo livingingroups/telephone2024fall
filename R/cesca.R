0951b8b35793beed4c7fc2ad49a3b3668085136c
FrancescaFrisoni <124571317+FrancescaFrisoni@users.noreply.github.com>
2024-11-19 14:13:06 +0100

    final version

--- R/cesca.R
+++ R/cesca.R
@@ -2,22 +2,21 @@ cesca_function <- function(word){
 
   input_index <- which(all_english_words == word)
 
-
+  # if it is a real word
   if(length(input_index) == 0) {
     print("Bummer, not a word")
   } else {
     print("Word found in list")
   }
 
+  # print a message for user
   if(length(input_index) != 0) {
     print(paste0(word," is a great word! What about ", all_english_words[input_index + 1], "? And all the other words that start with ", word, "?"))
   }
 
-  # all words that start with the given word
+  # retunr all words that start with the given word
   matching_words <- all_english_words[startsWith(all_english_words, word)]
 
-  # list of matching words
   return(matching_words)
-
 
 }
