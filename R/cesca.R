9e9d3a1e3c1b22cbf0df088a1d6d25f839e4d24f
FrancescaFrisoni <124571317+FrancescaFrisoni@users.noreply.github.com>
2024-11-19 13:37:44 +0100

    tried committ

--- R/cesca.R
+++ R/cesca.R
@@ -1,3 +1,13 @@
 cesca_function <- function(word){
-  return(word)
+
+  # Find input word in wordlist
+  input_index <- which(all_english_words == word)
+
+
+  if(length(input_index) == 0) {
+    print("Bummer, not English")
+  } else {
+    print("Word found in list, congrats!")
+  }
+
 }
