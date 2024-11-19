27a7c420c665aecafe6108ccc30985f77a380029
FrancescaFrisoni <124571317+FrancescaFrisoni@users.noreply.github.com>
2024-11-19 14:48:06 +0100

    changed as Kat

--- R/cesca.R
+++ R/cesca.R
@@ -2,21 +2,23 @@ cesca_function <- function(word){
 
   input_index <- which(all_english_words == word)
 
-  # if it is a real word
-  if(length(input_index) == 0) {
-    print("Bummer, not a word")
-  } else {
-    print("Word found in list")
-  }
+  # # if it is a real word
+  # if(length(input_index) == 0) {
+  #   print("Bummer, not a word")
+  # } else {
+  #   print("Word found in list")
+  # }
 
   # print a message for user
-  if(length(input_index) != 0) {
-    print(paste0(word," is a great word! What about ", all_english_words[input_index + 1], "? And all the other words that start with ", word, "?"))
-  }
+  # if(length(input_index) != 0) {
+  #   print(paste0(word," is a great word! What about ", all_english_words[input_index + 1], "? And all the other words that start with ", word, "?"))
+  # }
 
-  # return all words that start with the given word
+  if(length(input_index) != 0) {
+  # all words that start with the given word
   matching_words <- all_english_words[startsWith(all_english_words, word)]
 
-  return(matching_words)
-
+  # return the last-1 word that starts with word
+  return(matching_words[length(matching_words)-1])
+  }
 }
